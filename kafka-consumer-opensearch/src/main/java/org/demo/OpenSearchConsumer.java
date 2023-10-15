63d914b209d57b272986e129dae2d5d624dcb50e
Furkan AYDIN <furknydin@gmail.com>
2023-10-15 16:01:17 +0300

    openSearchClient is created and openSearch index is exist

--- kafka-consumer-opensearch/src/main/java/org/demo/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/org/demo/OpenSearchConsumer.java
@@ -1,7 +1,81 @@
 package org.demo;
 
-public class OpenSearchConsumer {
-    public static void main(String[] args) {
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URI;
+
+public class OpenSearchConsumer {
+    public static void main(String[] args) throws IOException {
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+
+        //create OpenSearch client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        //we need to create index on openSearch if it does not exist already
+        try(openSearchClient){
+            boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
+
+            if (!indexExist) {
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("The Wikimedia index has been created");
+            }else{
+                log.info("The Wikimedia index has been created");
+            }
+        }
+
+
+
+        //create kafka client
+
+        //main code logic
+
+        //close thing
+
+    }
+
+    public static RestHighLevelClient createOpenSearchClient(){
+        String connectionUrl = "https://qhi6vcf9tf:f05jh9dbbm@kafka-example-8650398864.eu-west-1.bonsaisearch.net:443";
+
+        //we build a URI from connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connectionUrl);
+
+        //extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+        return restHighLevelClient;
     }
 }
